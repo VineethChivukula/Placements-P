2b138f1b491d5fd1a83884b66315fe77e5babf4b
vineethchivukula <qualityhacker2002@gmail.com>
2023-02-19 15:14:27 +0530

    commit

---
+++ C/Chapter1/comments.c
@@ -0,0 +1,9 @@
+/*
+    This is a multi-line comment
+*/
+#include<stdio.h>
+int main(){
+    //This is a hello world program
+    printf("Hello World");
+    return 0;
+}
---
+++ C/Chapter1/output.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    printf("Hello C \n");
+    printf("Hello C \n");
+    printf("Hello C \n");
+    printf("Hello C \n");
+    int age=22;
+    float pi=3.14;
+    char star='*';
+    printf("Age is %d \n",age);
+    printf("Value of pi is %f \n",pi);
+    printf("Star looks like this %c \n",star);
+    return 0;
+}
